8e6f1d7d6b5e9abd6181aff045ac3c959bd72b4f
equmoretdia <serhiy.belinskyy@gmail.com>
2023-12-25 20:31:22 +0000

    Deploying to gh-pages from @ equmoretdia/goit-react-hw-05-movies@e4de5b23933dc45724cd4bbee3577a7781b31be3 🚀

---
+++ static/js/884.80b4f562.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[884],{884:function(e,i,t){t.r(i);var s=t(184);i.default=function(){return(0,s.jsx)("div",{children:"Movie Details page"})}}}]);
+//# sourceMappingURL=884.80b4f562.chunk.js.map
