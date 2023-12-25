11e93c37fdc61b45bdac3d775754b1839bee907e
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2023-12-25 18:09:43 +0100

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,24 @@
-export default function App() {
-  return <></>;
-}
+import Layout from './Layout';
+import Cast from './Cast';
+import Reviews from './Reviews';
+
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+};
